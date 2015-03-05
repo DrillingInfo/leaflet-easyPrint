cf347971d9b664e8fd7b88201b3e332aaddd5f32
Chad Hutcherson <chad.hutcherson@drillinginfo.com>
2015-03-05 16:28:51 -0600

    made map div configurable;hardcoded path to css file

--- dist/leaflet.easyPrint.js
+++ dist/leaflet.easyPrint.js
@@ -1,28 +1,30 @@
-
-
 L.Control.EasyPrint = L.Control.extend({
-    options: {
-        position: 'topleft',
-        title: 'Print map',
-    },
 
-    onAdd: function () {
-        var container = L.DomUtil.create('div', 'leaflet-control-easyPrint leaflet-bar leaflet-control');
+  options: {
+    position: 'bottomright',
+    title: 'Print map',
+  },
 
-        this.link = L.DomUtil.create('a', 'leaflet-control-easyPrint-button leaflet-bar-part', container);
-        this.link.href = 'javascript:void(0)($("#map").print({stylesheet:"easyPrint.css"}))';
+  initialize: function(mapId){
+    this.mapDiv = mapId;
+  },
 
-        return container;
-    },
-    
-  
-    _click: function (e) {
-        L.DomEvent.stopPropagation(e);
-        L.DomEvent.preventDefault(e);
-    },
+  onAdd: function () {
+    var container = L.DomUtil.create('div', 'leaflet-control-easyPrint leaflet-bar leaflet-control');
+
+    this.link = L.DomUtil.create('a', 'leaflet-control-easyPrint-button leaflet-bar-part', container);
+    this.link.href = "javascript:void(0)($('#" + this.mapDiv + "').print({stylesheet:'../bower_components/leaflet.easyPrint/dist/easyPrint.css'}))";
+
+    return container;
+  },
+
+
+  _click: function (e) {
+    L.DomEvent.stopPropagation(e);
+    L.DomEvent.preventDefault(e);
+  },
 });
 
-L.easyPrint = function() {
-  return new L.Control.EasyPrint();
+L.easyPrint = function(mapId) {
+  return new L.Control.EasyPrint(mapId);
 };
-
